351d6a9c8f287915381606099c2efe6028f8bb3d
Matt Reed <4d5452@gmail.com>
2017-06-26 17:56:16 -0400

    refactoring and adding http 500 status code support

--- server.js
+++ server.js
@@ -16,27 +16,36 @@ const log = bunyan.createLogger(bunyan_config);
 
 app.use(helmet());
 app.use('', server_routes);
-
-/* 404 response handler */
-app.use(function (req, res, next) {
-  log.warn(
-    {
-      req: {
-        ip: req.ip,
-        method: req.method,
-        url: req.originalUrl
-      }
-    }
-  );
-  res.status(404).sendFile(path.join(__dirname, '404.html'));
-});
-
-/* Error Handler */
-app.use(function (err, req, res, next) {
-  log.error({err: err});
-});
+app.use(notFoundHandler);
+app.use(logError);
+app.use(errorHandler);
 
 /* Start the nodejs application */
 app.listen(server_config.port, server_config.host, function() {
   log.info('Started');
 });
+
+/* 404 response handler */
+function notFoundHandler(req, res, next) {
+  log.warn({ req: getRequestLog(req) });
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
+};
+
+/* Error Handler */
+function errorHandler(err, req, res, next) {
+  if(res.headersSent) {
+    return next(err);
+  }
+  res.status(500).sendFile(path.join(__dirname, '500.html'));
+};
+
+/* Log Error */
+function logError(err, req, res, next) {
+  log.error({req: getRequestLog(req), err: err});
+  next(err);
+};
+
+/* Return simplified express req */
+function getRequestLog(req) {
+  return { 'ip': req.ip, 'method': req.method, 'url': req.originalUrl }
+};
