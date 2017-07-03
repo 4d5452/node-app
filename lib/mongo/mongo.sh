fd94bdda0c892198208c0b0327796198983758e2
Matt Reed <4d5452@gmail.com>
2017-07-03 01:08:37 -0400

    add verbose feature

--- lib/mongo/mongo.sh
+++ lib/mongo/mongo.sh
@@ -6,6 +6,7 @@ RESET=0
 COUNT=0
 STARTED=$(service mongod status | grep -e "Active: active")
 HELP=0
+VERBOSE=0
 MONGO_LOG=/var/log/mongodb/mongod.log
 MONGO_CONFIG=/etc/mongod.conf
 PORT=$(grep -e "port:.*" < $MONGO_CONFIG | awk '{ print $2; }')
@@ -27,6 +28,9 @@ while [[ $# -gt 0 ]]; do
       RESET=1
       ((COUNT++))
       ;;
+    -v|--verbose)
+      VERBOSE=1
+      ;;
     *)
       echo "Unknown option '$key'"
       HELP=1
@@ -41,11 +45,19 @@ print_help () {
   echo "--start := start the mongo service";
   echo "--stop := stop the mongo service";
   echo "--reset := reset the mongo service";
+  echo "Optional arguments";
+  echo "-v | --verbose := print mongo service status"
 }
 
-if [ "$HELP" -eq 1 ] || [ "$COUNT" -ne 1 ]; then
+# catch misuse of args
+if [ "$COUNT" -ne 1 ]; then
   print_help
-  exit
+  exit 1
+fi
+
+if [ "$HELP" -eq 1 ]; then
+  print_help
+  exit 0
 fi
 
 if [ "$START" -eq 1 ]; then
@@ -53,12 +65,10 @@ if [ "$START" -eq 1 ]; then
   if [ -n "$STARTED" ]; then
     # length is not-zero: service already started
     echo "Error: mongo is currently running."
-    exit
+    exit 1
   fi
   # start the mongo service
-  echo "Starting mongo"
   sudo service mongod start
-  service mongod status
 fi
 
 if [ "$STOP" -eq 1 ]; then
@@ -66,11 +76,9 @@ if [ "$STOP" -eq 1 ]; then
   if [ -z "$STARTED" ]; then
     # length is zero: service is not started
     echo "Error: mongo is not running."
-    exit
+    exit 1
   fi
-  echo "Stopping mongo"
   sudo service mongod stop
-  service mongod status
 fi
 
 if [ "$RESET" -eq 1 ]; then
@@ -78,11 +86,14 @@ if [ "$RESET" -eq 1 ]; then
   if [ -z "$STARTED" ]; then
     # length is zero: service is not running
     echo "Error: mongo is not running."
-    exit
+    exit 1
   fi
-  echo "Reseting the mongo service"
   sudo service mongod restart
+fi
+
+if [ "$VERBOSE" -eq 1 ]; then
+  # show server status
   service mongod status
 fi
 
-echo "Fin"
+exit 0
